1d4fe052d1358cdd3576571547eeb51db618b2f8
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-03-08 15:24:11 +0100

    added some comments

--- src/Game.tsx
+++ src/Game.tsx
@@ -5,33 +5,35 @@ import { createContext, useEffect, useState } from "react";
 export const PlayerContext = createContext<number[]>([]);
 
 export function Game() {
+    // starting player coordinates
     const [coordinate, setCoordinate] = useState([1, 1]);
 
+    // updates player coordinates on keypress, eventListener is added and removed on render
     useEffect(() => {
         const calculateCoordinate = (x: number, y: number) => {
             if (x) {
-                x = Math.min(Math.max((coordinate[1] + x), 1), 10)
+                x = Math.min(Math.max(coordinate[1] + x, 1), 10);
             } else {
-                x = coordinate[1]
+                x = coordinate[1];
             }
             if (y) {
-                y = Math.min(Math.max((coordinate[0] + y), 1), 10)
+                y = Math.min(Math.max(coordinate[0] + y, 1), 10);
             } else {
-                y = coordinate[0]
+                y = coordinate[0];
             }
-            console.log(x, y)
-            return [y, x]
-        }
+            console.log(x, y);
+            return [y, x];
+        };
         const keyPress = (e: KeyboardEvent) => {
             console.log(e.code);
             if (e.code === "ArrowUp" || e.code === "KeyW") {
-                setCoordinate(calculateCoordinate(0, -1))
+                setCoordinate(calculateCoordinate(0, -1));
             } else if (e.code === "ArrowDown" || e.code === "KeyS") {
-                setCoordinate(calculateCoordinate(0, 1))
+                setCoordinate(calculateCoordinate(0, 1));
             } else if (e.code === "ArrowRight" || e.code === "KeyD") {
-                setCoordinate(calculateCoordinate(1, 0))
+                setCoordinate(calculateCoordinate(1, 0));
             } else if (e.code === "ArrowLeft" || e.code === "KeyA") {
-                setCoordinate(calculateCoordinate(-1, 0))
+                setCoordinate(calculateCoordinate(-1, 0));
             }
         };
         window.addEventListener("keydown", keyPress);
