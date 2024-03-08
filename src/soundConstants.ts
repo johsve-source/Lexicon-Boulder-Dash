02dde4b01f1b06406190bb694214e2b8b42ec47a
Johan Svensson <johsve@protonmail.com>
2024-03-08 15:05:24 +0100

    Sound Constants

--- src/soundConstants.ts
+++ src/soundConstants.ts
@@ -1,16 +1,19 @@
+// Default game sound duration
+export const DEFAULT_DURATION = 3000;
+
 // Main sound files
-export const START_GAME = 'start-game.mp3';
-export const GAME_OVER = 'game-over.mp3';
-export const UI_INTERACTION = 'ui-interaction.mp3';
+export const START_GAME = './constants/sounds/start-game.mp3';
+export const GAME_OVER = './constants/sounds/game-over.mp3';
+export const UI_INTERACTION = './constants/sounds/ui-interaction.mp3';
 
 // Game sound files
-export const AMBIANCE = 'ambiance.mp3';
-export const DIGGING_DIRT = 'digging-dirt.mp3';
-export const FALLING_STONE = 'falling-stone.mp3';
-export const STONE_EXPLODE = 'stone-explode.mp3';
-export const COLLECTING_DIAMOND = 'collecting-diamond.mp3';
-export const COLLECTING_POWERUP = 'collecting-powerup.mp3';
+export const AMBIANCE = './constants/sounds/ambiance.mp3';
+export const DIGGING_DIRT = './constants/sounds/digging-dirt.mp3';
+export const FALLING_STONE = './constants/sounds/falling-stone.mp3';
+export const STONE_EXPLODE = './constants/sounds/stone-explode.mp3';
+export const COLLECTING_DIAMOND = './constants/sounds/collecting-diamond.mp3';
+export const COLLECTING_POWERUP = './constants/sounds/collecting-powerup.mp3';
 
 // Change Level sound files
-export const GAME_LEVEL_2 = 'game-level-2.mp3';
-export const GAME_LEVEL_3 = 'game-level-3.mp3';
+export const GAME_LEVEL_2 = './constants/sounds/game-level-2.mp3';
+export const GAME_LEVEL_3 = './constants/sounds/game-level-3.mp3';
