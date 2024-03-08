a6f9df275e2d52de69d6bf12cb3fd6c4f14cf699
Johan Svensson <johsve@protonmail.com>
2024-03-08 14:13:26 +0100

    Added Sound Constants

---
+++ src/soundConstants.ts
@@ -0,0 +1,16 @@
+// Main sound files
+export const START_GAME = 'start-game.mp3';
+export const GAME_OVER = 'game-over.mp3';
+export const UI_INTERACTION = 'ui-interaction.mp3';
+
+// Game sound files
+export const AMBIANCE = 'ambiance.mp3';
+export const DIGGING_DIRT = 'digging-dirt.mp3';
+export const FALLING_STONE = 'falling-stone.mp3';
+export const STONE_EXPLODE = 'stone-explode.mp3';
+export const COLLECTING_DIAMOND = 'collecting-diamond.mp3';
+export const COLLECTING_POWERUP = 'collecting-powerup.mp3';
+
+// Change Level sound files
+export const GAME_LEVEL_2 = 'game-level-2.mp3';
+export const GAME_LEVEL_3 = 'game-level-3.mp3';
