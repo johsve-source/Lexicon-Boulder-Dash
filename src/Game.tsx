08b8511ee98139de8ed907f8c00c89f48597652a
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-03-08 15:15:41 +0100

    improved movement logic

--- src/Game.tsx
+++ src/Game.tsx
@@ -8,22 +8,36 @@ export function Game() {
     const [coordinate, setCoordinate] = useState([1, 1]);
 
     useEffect(() => {
+        const calculateCoordinate = (x: number, y: number) => {
+            if (x) {
+                x = Math.min(Math.max((coordinate[1] + x), 1), 10)
+            } else {
+                x = coordinate[1]
+            }
+            if (y) {
+                y = Math.min(Math.max((coordinate[0] + y), 1), 10)
+            } else {
+                y = coordinate[0]
+            }
+            console.log(x, y)
+            return [y, x]
+        }
         const keyPress = (e: KeyboardEvent) => {
             console.log(e.code);
             if (e.code === "ArrowUp" || e.code === "KeyW") {
-                setCoordinate([coordinate[0] - 1, coordinate[1]]);
+                setCoordinate(calculateCoordinate(0, -1))
             } else if (e.code === "ArrowDown" || e.code === "KeyS") {
-                setCoordinate([coordinate[0] + 1, coordinate[1]]);
+                setCoordinate(calculateCoordinate(0, 1))
             } else if (e.code === "ArrowRight" || e.code === "KeyD") {
-                setCoordinate([coordinate[0], coordinate[1] + 1]);
+                setCoordinate(calculateCoordinate(1, 0))
             } else if (e.code === "ArrowLeft" || e.code === "KeyA") {
-                setCoordinate([coordinate[0], coordinate[1] - 1]);
+                setCoordinate(calculateCoordinate(-1, 0))
             }
         };
-        window.addEventListener("keyup", keyPress);
+        window.addEventListener("keydown", keyPress);
 
         return () => {
-            window.removeEventListener("keyup", keyPress);
+            window.removeEventListener("keydown", keyPress);
         };
     }, [coordinate]);
 
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -6,7 +6,7 @@ function Player() {
     const coordinate = useContext(PlayerContext);
     return (
         <>
-            <img src="/player.png" alt="The Player" className="Player" style={{ gridArea: `${Math.max(1, (coordinate[0] % 10) + 1)} / ${Math.max((coordinate[1] % 10) + 1)}` }} />
+            <img src="/player.png" alt="The Player" className="Player" style={{ gridArea: `${coordinate[0]} / ${coordinate[1]}` }} />
         </>
     );
 }
