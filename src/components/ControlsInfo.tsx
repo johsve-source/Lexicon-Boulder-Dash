235e7c40314f15f2ddfcf6eebd446dbb4f148ce3
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-03-11 15:07:42 +0100

    Finished ControlsInfo component.

--- src/components/ControlsInfo.tsx
+++ src/components/ControlsInfo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef } from "react"
 import "./ControlsInfo.css"
 
 const controls = {
@@ -9,31 +9,63 @@ const controls = {
 }
 
 function ControlsInfo() {
-	const [visible, setVisible] = useState(false)
+	const buttonRef = useRef<HTMLButtonElement>(null)
+	const dialogRef = useRef<HTMLDialogElement>(null)
 
 	return (
-		<button
-			className={visible ? "controls-info visible" : "controls-info"}
-			onClick={() => {
-				setVisible(!visible)
-			}}
-		>
-			Controls
-			<div className="info">
+		<>
+			<button
+				ref={buttonRef}
+				className="controls-info"
+				onClick={() => {
+					if (!dialogRef.current) return
+					dialogRef.current.showModal()
+					if (!buttonRef.current) return
+
+					const rect = buttonRef.current.getBoundingClientRect()
+					dialogRef.current.style.left = rect.x + "px"
+					dialogRef.current.style.top = `calc(${
+						rect.y + rect.height
+					}px + 0.25em)`
+				}}
+			>
+				Controls
+			</button>
+
+			<dialog
+				ref={dialogRef}
+				className="controls-info"
+				onClick={e => {
+					if (!dialogRef.current) return
+
+					const rect = dialogRef.current.getBoundingClientRect()
+					const clickedInDialog =
+						rect.top <= e.clientY &&
+						e.clientY <= rect.top + rect.height &&
+						rect.left <= e.clientX &&
+						e.clientX <= rect.left + rect.width
+
+					if (!clickedInDialog) dialogRef.current.close()
+				}}
+			>
 				<table>
-					{Object.entries(controls).map(([maping, keys], index) => (
-						<tr key={index}>
-							<td>{maping}:</td>
-							{keys.map(key => (
-								<td>
-									<b>{key}</b>
-								</td>
-							))}
-						</tr>
-					))}
+					<tbody>
+						{Object.entries(controls).map(
+							([maping, keys], index) => (
+								<tr key={index}>
+									<td>{maping}:</td>
+									{keys.map(key => (
+										<td key={key}>
+											<b>{key}</b>
+										</td>
+									))}
+								</tr>
+							)
+						)}
+					</tbody>
 				</table>
-			</div>
-		</button>
+			</dialog>
+		</>
 	)
 }
 
