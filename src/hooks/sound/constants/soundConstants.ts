b0f64684f70490a2868ea5940147bb3494445602
Johan Svensson <johsve@protonmail.com>
2024-03-11 13:55:57 +0100

    Added soundManager functionality

---
+++ src/hooks/sound/constants/soundConstants.ts
@@ -0,0 +1,19 @@
+// Default game sound duration
+export const DEFAULT_DURATION = 3000;
+
+// Main sound files
+export const START_GAME = '/constants/sounds/start-game.mp3';
+export const GAME_OVER = '/constants/sounds/game-over.mp3';
+export const UI_INTERACTION = '/constants/sounds/ui-interaction.mp3';
+
+// Game sound files
+export const AMBIANCE = '/constants/sounds/ambiance.mp3';
+export const DIGGING_DIRT = '/constants/sounds/digging.mp3';
+export const FALLING_STONE = '/constants/sounds/falling-stones.mp3';
+export const STONE_EXPLODE = '/constants/sounds/stone-impact.mp3';
+export const COLLECTING_DIAMOND = '/constants/sounds/collecting-diamonds.mp3';
+export const COLLECTING_POWERUP = '/constants/sounds/collecting-powerups.mp3';
+
+// Change Level sound files
+export const GAME_LEVEL_2 = '/constants/sounds/game-level-2.mp3';
+export const GAME_LEVEL_3 = '/constants/sounds/game-level-3.mp3';
