1c9d754fb8a9ad3f4c0527cf9e348395d2841c4f
Johan <johsve@protonmail.com>
2024-03-12 12:58:27 +0000

    Added the ability to dig dirt and a finish block where you switch level
    Co-authored-by: Marcus <ProgrammerMarcus@users.noreply.github.com>
    Co-authored-by: Jonatan Drysén <JonatanDrysen@users.noreply.github.com>

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,12 @@
-import "./Game.css";
-import { createContext, useEffect, useRef, useState } from "react";
-import Block from "./components/Generic";
+import './Game.css'
+import { createContext, useEffect, useRef, useState } from 'react'
+import Block from './components/Generic'
 
-export const PlayerContext = createContext<number[]>([]);
+export const PlayerContext = createContext<number[]>([])
 
 export function Game() {
-    // prettier-ignore
-    const [blocks, setBlocks] = useState([
+  // prettier-ignore
+  const [blocks, setBlocks] = useState([
         "b", "b", "b", "b", "b", "b", "b", "b", "b", "b",
         "b", "p", "d", "d", "d", "d", "d", "d", "d", "b",
         "b", "d", "d", "d", "d", "d", "d", "d", "d", "b",
@@ -15,77 +15,80 @@ export function Game() {
         "b", "d", "d", "d", "d", "d", "i", "d", "d", "b",
         "b", "d", "d", "d", "d", "d", "d", "d", "d", "b",
         "b", "s", "s", "s", "d", "d", "d", "d", "d", "b",
-        "b", "d", "d", "d", "d", "d", "d", "d", "d", "b",
+        "b", "d", "d", "d", "d", "d", "d", "d", "f", "b",
         "b", "b", "b", "b", "b", "b", "b", "b", "b", "b",
     ]);
 
-    // starting player coordinates
-    const position = useRef(11);
+  // starting player coordinates
+  const position = useRef(11)
 
-    // updates player coordinates on keypress, eventListener is added and removed on render
-    useEffect(() => {
-        const keyPress = (e: KeyboardEvent) => {
-            console.log(e.code);
-            if (e.code === "ArrowUp" || e.code === "KeyW") {
-                const newBlocks = [...blocks];
-                const copy = newBlocks[position.current - 10];
-                newBlocks[position.current - 10] = newBlocks[position.current];
-                newBlocks[position.current] = copy;
-                position.current -= 10;
-                setBlocks(newBlocks);
-            } else if (e.code === "ArrowDown" || e.code === "KeyS") {
-                const newBlocks = [...blocks];
-                const copy = newBlocks[position.current + 10];
-                newBlocks[position.current + 10] = newBlocks[position.current];
-                newBlocks[position.current] = copy;
-                position.current += 10;
-                setBlocks(newBlocks);
-            } else if (e.code === "ArrowRight" || e.code === "KeyD") {
-                const newBlocks = [...blocks];
-                const copy = newBlocks[position.current + 1];
-                newBlocks[position.current + 1] = newBlocks[position.current];
-                newBlocks[position.current] = copy;
-                position.current += 1;
-                setBlocks(newBlocks);
-            } else if (e.code === "ArrowLeft" || e.code === "KeyA") {
-                const newBlocks = [...blocks];
-                const copy = newBlocks[position.current - 1];
-                newBlocks[position.current - 1] = newBlocks[position.current];
-                newBlocks[position.current] = copy;
-                position.current -= 1;
-                setBlocks(newBlocks);
-            }
-        };
-        window.addEventListener("keydown", keyPress);
-
-        return () => {
-            window.removeEventListener("keydown", keyPress);
-        };
-    }, [blocks]);
-
-    function toImagePath(type: string) {
-        if (type === "b") {
-            return "/bedrock.png";
-        } else if (type === "d") {
-            return "/dirt.png";
-        } else if (type === "s") {
-            return "/stone.png";
-        } else if (type === "i") {
-            return "/diamond.png";
-        } else if (type === "p") {
-            return "/player.png";
-        } else {
-            return "/player.png";
+  // updates player coordinates on keypress, eventListener is added and removed on render
+  useEffect(() => {
+    const keyPress = (e: KeyboardEvent) => {
+      console.log(e.code)
+      function handleMove(delta: number) {
+        const newBlocks = [...blocks]
+        const copy = newBlocks[position.current + delta]
+        if (copy === 'b') {
+          return
+        } else if (copy === 'd' || copy === 'n') {
+          newBlocks[position.current + delta] = newBlocks[position.current]
+          newBlocks[position.current] = 'n'
+          position.current += delta
+          setBlocks(newBlocks)
+        } else if (copy === 'f') {
+          alert('WE HAVE A WINNER!')
         }
+      }
+      if (e.code === 'ArrowUp' || e.code === 'KeyW') {
+        handleMove(-10)
+      } else if (e.code === 'ArrowDown' || e.code === 'KeyS') {
+        handleMove(10)
+      } else if (e.code === 'ArrowRight' || e.code === 'KeyD') {
+        handleMove(1)
+      } else if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
+        handleMove(-1)
+      }
     }
+    window.addEventListener('keydown', keyPress)
 
-    return (
-        <div className="Game">
-            {blocks.map((key, index) => (
-                <Block key={index} x={(index + 1) % 10} y={(index + 1) / 10} image={toImagePath(key)} />
-            ))}
-        </div>
-    );
+    return () => {
+      window.removeEventListener('keydown', keyPress)
+    }
+  }, [blocks])
+
+  function toImagePath(type: string) {
+    if (type === 'b') {
+      return '/bedrock.png'
+    } else if (type === 'd') {
+      return '/dirt.png'
+    } else if (type === 's') {
+      return '/stone.png'
+    } else if (type === 'i') {
+      return '/diamond.png'
+    } else if (type === 'p') {
+      return '/player.png'
+    } else if (type === 'n') {
+      return '/none.png'
+    } else if (type === 'f') {
+      return '/finish.png'
+    } else {
+      return '/player.png'
+    }
+  }
+
+  return (
+    <div className="Game">
+      {blocks.map((key, index) => (
+        <Block
+          key={index}
+          x={(index + 1) % 10}
+          y={(index + 1) / 10}
+          image={toImagePath(key)}
+        />
+      ))}
+    </div>
+  )
 }
 
-export default Game;
+export default Game
