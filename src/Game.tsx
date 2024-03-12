6784ffd1b6ce7bff9f397cf47f1d15a0af45a80b
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-03-12 10:07:31 +0100

    swap movement implemented

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,48 +1,11 @@
 import "./Game.css";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import Block from "./components/Generic";
 
 export const PlayerContext = createContext<number[]>([]);
 
 export function Game() {
-    // starting player coordinates
-    const [coordinate, setCoordinate] = useState([1, 1]);
-
-    // updates player coordinates on keypress, eventListener is added and removed on render
-    useEffect(() => {
-        const calculateCoordinate = (x: number, y: number) => {
-            if (x) {
-                x = Math.min(Math.max(coordinate[1] + x, 1), 10);
-            } else {
-                x = coordinate[1];
-            }
-            if (y) {
-                y = Math.min(Math.max(coordinate[0] + y, 1), 10);
-            } else {
-                y = coordinate[0];
-            }
-            console.log(x, y);
-            return [y, x];
-        };
-        const keyPress = (e: KeyboardEvent) => {
-            console.log(e.code);
-            if (e.code === "ArrowUp" || e.code === "KeyW") {
-                setCoordinate(calculateCoordinate(0, -1));
-            } else if (e.code === "ArrowDown" || e.code === "KeyS") {
-                setCoordinate(calculateCoordinate(0, 1));
-            } else if (e.code === "ArrowRight" || e.code === "KeyD") {
-                setCoordinate(calculateCoordinate(1, 0));
-            } else if (e.code === "ArrowLeft" || e.code === "KeyA") {
-                setCoordinate(calculateCoordinate(-1, 0));
-            }
-        };
-        window.addEventListener("keydown", keyPress);
-
-        return () => {
-            window.removeEventListener("keydown", keyPress);
-        };
-    }, [coordinate]);
-
+    // prettier-ignore
     const [blocks, setBlocks] = useState([
         "b", "b", "b", "b", "b", "b", "b", "b", "b", "b",
         "b", "p", "d", "d", "d", "d", "d", "d", "d", "b",
@@ -56,35 +19,72 @@ export function Game() {
         "b", "b", "b", "b", "b", "b", "b", "b", "b", "b",
     ]);
 
+    // starting player coordinates
+    const position = useRef(11);
+
+    // updates player coordinates on keypress, eventListener is added and removed on render
+    useEffect(() => {
+        const keyPress = (e: KeyboardEvent) => {
+            console.log(e.code);
+            if (e.code === "ArrowUp" || e.code === "KeyW") {
+                const newBlocks = [...blocks];
+                const copy = newBlocks[position.current - 10];
+                newBlocks[position.current - 10] = newBlocks[position.current];
+                newBlocks[position.current] = copy;
+                position.current -= 10;
+                setBlocks(newBlocks);
+            } else if (e.code === "ArrowDown" || e.code === "KeyS") {
+                const newBlocks = [...blocks];
+                const copy = newBlocks[position.current + 10];
+                newBlocks[position.current + 10] = newBlocks[position.current];
+                newBlocks[position.current] = copy;
+                position.current += 10;
+                setBlocks(newBlocks);
+            } else if (e.code === "ArrowRight" || e.code === "KeyD") {
+                const newBlocks = [...blocks];
+                const copy = newBlocks[position.current + 1];
+                newBlocks[position.current + 1] = newBlocks[position.current];
+                newBlocks[position.current] = copy;
+                position.current += 1;
+                setBlocks(newBlocks);
+            } else if (e.code === "ArrowLeft" || e.code === "KeyA") {
+                const newBlocks = [...blocks];
+                const copy = newBlocks[position.current - 1];
+                newBlocks[position.current - 1] = newBlocks[position.current];
+                newBlocks[position.current] = copy;
+                position.current -= 1;
+                setBlocks(newBlocks);
+            }
+        };
+        window.addEventListener("keydown", keyPress);
+
+        return () => {
+            window.removeEventListener("keydown", keyPress);
+        };
+    }, [blocks]);
+
     function toImagePath(type: string) {
         if (type === "b") {
-            return "/bedrock.png"
-        }
-        else if (type === "d") {
-            return "/dirt.png"
-        }
-        else if (type === "s") {
-            return "/stone.png"
-        }
-        else if (type === "i") {
-            return "/diamond.png"
-        }
-        else if (type === "p") {
-            return "/player.png"
-        }
-        else {
-            return "/player.png"
+            return "/bedrock.png";
+        } else if (type === "d") {
+            return "/dirt.png";
+        } else if (type === "s") {
+            return "/stone.png";
+        } else if (type === "i") {
+            return "/diamond.png";
+        } else if (type === "p") {
+            return "/player.png";
+        } else {
+            return "/player.png";
         }
     }
 
-
-
     return (
-        <PlayerContext.Provider value={coordinate}>
-            <div className="Game">
-                {blocks.map((key, index) => <Block key={index} x={((index + 1) % 10)} y={(index + 1) / 10} image={toImagePath(key)} />)}
-            </div>
-        </PlayerContext.Provider>
+        <div className="Game">
+            {blocks.map((key, index) => (
+                <Block key={index} x={(index + 1) % 10} y={(index + 1) / 10} image={toImagePath(key)} />
+            ))}
+        </div>
     );
 }
 
