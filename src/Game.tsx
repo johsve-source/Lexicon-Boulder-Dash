55cc640c6d5b7e62e9ac8fb8f8f368dc8d349145
CarinWood <carin.wood.85@gmail.com>
2024-03-20 16:05:37 +0100

    update end function

--- src/Game.tsx
+++ src/Game.tsx
@@ -21,9 +21,11 @@ export function Game() {
   const [gameState, gameDispatch] = GetGameReducer()
 
   function handleEnterNameClick() {
-    console.log('hello')
     setEnterNameVisible(true)
-    console.log(isEnterNameVisible)
+  }
+
+  function setNameClickFalse() {
+    setEnterNameVisible(false)
   }
 
   function handlePlayClick() {
@@ -175,7 +177,7 @@ export function Game() {
           highscores={highscoreTestData}
           handleEnterNameClick={handleEnterNameClick}
         />
-      ) : isEnterNameVisible ? <NameInput /> 
+      ) : isEnterNameVisible ? <NameInput setNameClickFalse={setNameClickFalse}/> 
       :(
         <div
           className="Game"
