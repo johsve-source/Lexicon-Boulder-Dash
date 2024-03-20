d5fcdc43390f0547b64f45dc4e882b40b7864b77
CarinWood <carin.wood.85@gmail.com>
2024-03-20 11:21:25 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css'
-import NameInput from './components/nameInput/NameInput'
-/* import Game from './Game'
-import { SpeedInsights } from '@vercel/speed-insights/react' */
+import Game from './Game'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 
 function App() {
   return (
     <>
-   {/*    <SpeedInsights />
-      <Game /> */}
-      <NameInput/>
+      <SpeedInsights />
+      <Game />
+   
     </>
   )
 }
