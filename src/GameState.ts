84198b33d160e96a281813c4c955adf205bc73a7
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-04-02 14:25:48 +0200

    Designed level 9.

--- src/GameState.ts
+++ src/GameState.ts
@@ -58,7 +58,7 @@ export function GetGameReducer(): [GameState, React.Dispatch<GameAction>] {
   const [gameState, gameDispatch] = useReducer(gameReducer, new GameState())
 
   useEffect(() => {
-    loadLevel(gameDispatch, 'level1')
+    loadLevel(gameDispatch, 'level9')
   }, [])
 
   return [gameState, gameDispatch]
