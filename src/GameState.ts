1c0c58117d357f2cf00223baed2663d519523746
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-04-02 15:02:20 +0200

    Working on levels.

--- src/GameState.ts
+++ src/GameState.ts
@@ -58,7 +58,7 @@ export function GetGameReducer(): [GameState, React.Dispatch<GameAction>] {
   const [gameState, gameDispatch] = useReducer(gameReducer, new GameState())
 
   useEffect(() => {
-    loadLevel(gameDispatch, 'level9')
+    loadLevel(gameDispatch, 'level10')
   }, [])
 
   return [gameState, gameDispatch]
