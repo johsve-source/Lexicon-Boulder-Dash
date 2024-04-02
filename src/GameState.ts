ef7e63dfa928ffeb9b6fb8db33efc506a154d275
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-04-02 11:34:18 +0200

    added level 12

--- src/GameState.ts
+++ src/GameState.ts
@@ -58,7 +58,7 @@ export function GetGameReducer(): [GameState, React.Dispatch<GameAction>] {
   const [gameState, gameDispatch] = useReducer(gameReducer, new GameState())
 
   useEffect(() => {
-    loadLevel(gameDispatch, 'level11')
+    loadLevel(gameDispatch, 'level12')
   }, [])
 
   return [gameState, gameDispatch]
