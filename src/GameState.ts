0bfd3bdc8e8d64a9ede3ff4a3406012cd6920802
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-04-03 14:32:49 +0200

    Adde grass texture for level 6 to 10.

--- src/GameState.ts
+++ src/GameState.ts
@@ -58,7 +58,7 @@ export function GetGameReducer(): [GameState, React.Dispatch<GameAction>] {
   const [gameState, gameDispatch] = useReducer(gameReducer, new GameState())
 
   useEffect(() => {
-    loadLevel(gameDispatch, 'level1')
+    loadLevel(gameDispatch, 'level6')
   }, [])
 
   return [gameState, gameDispatch]
